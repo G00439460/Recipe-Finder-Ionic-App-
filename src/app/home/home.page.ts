c54704dc1d88a6eadc7fe7f415c694e5084dd971
G00439460 <G00439460@atu.ie>
2025-12-16 21:55:25 +0000

    Continue working in the project, 1-Setting up the home page, 2-Adding the other pages: -Recipe details, -Setting page, -Favourite page. After tthat built the routes and linking the pages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonInput } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import {RouterLink} from '@angular/router';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent,
-    IonButton, IonButtons, IonIcon
+    IonButton, IonIcon, FormsModule, IonInput,
+    RouterLink,
+    
   ],
 })
 export class HomePage {
-  constructor() {}
+
+ingredients: string = '';
+
+  searchRecipes() {
+    // Split by commas, trim spaces
+    const ingredientList = this.ingredients
+      .split(',')
+      .map(i => i.trim())
+      .filter(i => i.length > 0);
+
+    console.log('Searching recipes with:', ingredientList);
+    // Here you can call your recipe API or service
+  }
 }
+
