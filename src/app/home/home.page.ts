430607756aca053edcfadbd1fb8c2790f66d7d5b
G00439460 <G00439460@atu.ie>
2025-12-16 18:30:00 +0000

    First commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,
+    IonButton, IonButtons, IonIcon
+  ],
+})
+export class HomePage {
+  constructor() {}
+}
